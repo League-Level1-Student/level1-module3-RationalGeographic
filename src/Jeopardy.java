71c0699f131891d7c4280c02ab19c37bbdb6fbf0
jennakimsd <league@WTS-iMac-11.attlocal.net>
2018-06-18 14:55:59 -0700

    movie/jp
    IMALIVE

--- src/Jeopardy.java
+++ src/Jeopardy.java
@@ -43,57 +43,71 @@ public class Jeopardy implements ActionListener {
 		JFrame frame = new JFrame();
 		quizPanel = new JPanel();
 		frame.setLayout(new BorderLayout());
-		
+
 		// 1. Make the frame show up
-frame.setVisible(true);
+	
 		// 2. Give your frame a title
 		frame.setTitle("Frame32");
 		// 3. Create a JPanel variable to hold the header using the createHeader method
-JPanel qpanel = new JPanel();
+		JPanel qpanel = new JPanel();
 		qpanel = createHeader("topic");
 		// 4. Add the header component to the quizPanel
 		quizPanel.add(qpanel);
 		// 5. Add the quizPanel to the frame
-frame.add(quizPanel);
+		frame.add(quizPanel);
+
+		// 6. Use the createButton method to set the value of firstButton
 		
-		// 6. Use the createButton method to set the value of firstButton 
-	createButton("200");
-	// 7. Add the firstButton to the quizPanel
-quizPanel.add(firstButton);		
-		// 8. Write the code inside the createButton() method below. Check that your game looks like Figure 1 in the Jeopardy Handout - http://bit.ly/1bvnvd4.
-		
-		// 9. Use the secondButton variable to hold a button using the createButton method
-		
+		// 7. Add the firstButton to the quizPanel
+		quizPanel.add(firstButton);
+		// 8. Write the code inside the createButton() method below. Check that your
+		// game looks like Figure 1 in the Jeopardy Handout - http://bit.ly/1bvnvd4.
+
+		// 9. Use the secondButton variable to hold a button using the createButton
+		// method
+		firstButton = createButton("200");
+		secondButton = createButton("200");
+		thirdButton = createButton("200");
+		fourthButton = createButton("200");
 		// 10. Add the secondButton to the quizPanel
 		
 		// 11. Add action listeners to the buttons (2 lines of code)
-	
-
+		firstButton.addActionListener(null);
+		secondButton.addActionListener(null);
 		// 12. Fill in the actionPerformed() method below
-				
+frame.setVisible(true);
 		frame.pack();
-		quizPanel.setLayout(new GridLayout(buttonCount+1, 3));
+		quizPanel.setLayout(new GridLayout(buttonCount + 1, 3));
 		frame.add(makeScorePanel(), BorderLayout.NORTH);
-		frame.setSize(Toolkit.getDefaultToolkit().getScreenSize().height, Toolkit.getDefaultToolkit().getScreenSize().width);
+		frame.setSize(Toolkit.getDefaultToolkit().getScreenSize().height,
+				Toolkit.getDefaultToolkit().getScreenSize().width);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
 
-	/*
-	 * 13. Use the method provided to play the Jeopardy theme music
-	 * 
-	 * 14. Add buttons so that you have $200, $400, $600, $800 and $1000 questions
-	 *
-	 * [optional] Use the showImage or playSound methods when the user answers a
-	 * question
-	 */
+		//
+		// 13. Use the method provided to play the Jeopardy theme music
+		//
+		//
+		playJeopardyTheme();
+		quizPanel.add(firstButton);
+		quizPanel.add(secondButton);
+		quizPanel.add(thirdButton);
+		quizPanel.add(fourthButton);
+		
+	
+	}
+	// 14. Add buttons so that you have $200, $400, $600, $800 and $1000 questions
+	//
+	// [optional] Use the showImage or playSound methods when the user answers a
+	// question
+	//
 
 	private JButton createButton(String dollarAmount) {
 		// Create a new JButton
-JButton buton = new JButton();
+		JButton buton = new JButton();
 		// Set the text of the button to the dollarAmount
-buton.setText(dollarAmount);
+		buton.setText(dollarAmount);
 		// Increment the buttonCount (this should make the layout vertical)
-buttonCount = buttonCount + 1;
+		buttonCount = buttonCount + 1;
 		// Return your new button instead of the temporary button
 
 		return (buton);
@@ -107,37 +121,46 @@ buttonCount = buttonCount + 1;
 
 		JButton buttonPressed = (JButton) arg0.getSource();
 		// If the buttonPressed was the firstButton
-
-		// Call the askQuestion() method
-
-		// Fill in the askQuestion() method. When you play the game, the score should
-		// change.
-
+		if (buttonPressed.equals(firstButton)) {
+			// Call the askQuestion() method
+			askQuestion("what is the capitol of alaska?", "juno", 200, score);
+			// Fill in the askQuestion() method. When you play the game, the score should
+			// change.
+		}
 		// Or if the buttonPressed was the secondButton
+		if (buttonPressed.equals(secondButton)) {
+			askQuestion("when  was lego city undercover first released?", "2013", 200, score);
 
+		}
 		// Call the askQuestion() method with a harder question
 
 		// Clear the button text (set the button text to nothing)
 
 	}
 
-	private void askQuestion(String question, String correctAnswer, int prizeMoney) {
+	public void askQuestion(String question, String correctAnswer, int prizeMoney, int score) {
 		// Remove this temporary message
-		JOptionPane.showMessageDialog(null, "this is where the question will be asked");
+		String question2 = JOptionPane.showInputDialog(question);
 		// Use a pop up to ask the user the question
 
 		// If the answer is correct
+		if (question2.equals(correctAnswer)) {
+			score = score += prizeMoney;
 
-		// Increase the score by the prizeMoney
+			// Increase the score by the prizeMoney
 
-		// Call the updateScore() method
-
-		// Pop up a message to tell the user they were correct
-
-		// Otherwise
-
+			// Call the updateScore() method
+			updateScore();
+			// Pop up a message to tell the user they were correct
+			JOptionPane.showMessageDialog(null, "You were correct!");
+			// Otherwise
+		}
 		// Decrement the score by the prizeMoney
-
+		else {
+			score = score -= prizeMoney;
+			JOptionPane.showMessageDialog(null, correctAnswer);
+			updateScore();
+		}
 		// Pop up a message to tell the user the correct answer
 
 		// Call the updateScore() method
---
+++ src/moviechill.java
@@ -0,0 +1,11 @@
+
+public class moviechill {
+public static void main(String[] args) {
+	
+	Movie mav1 = new Movie("slam", 10);
+	Movie mav2 = new Movie("slam", 10);
+	Movie mav3 = new Movie("slam", 10);
+	Movie mav4 = new Movie("slam", 10);
+	Movie mav5 = new Movie("slam", 10);
+}
+}
