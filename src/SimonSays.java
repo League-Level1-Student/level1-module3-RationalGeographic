34c47c467a6b9fa379290a3b175cd2b680dd69f1
RationalGeographic <league@WTS-IM-6.attlocal.net>
2018-06-20 14:25:41 -0700

    simon, wackamole
    true

--- src/SimonSays.java
+++ src/SimonSays.java
@@ -22,7 +22,7 @@ public class SimonSays extends KeyAdapter {
  
  // Complete steps 1 - 7 before you test
  // 1. Make a JFrame variable 
- 
+ JFrame frame = new JFrame();
  	HashMap<Integer,String> images = new HashMap<Integer, String>();
  	private int imageIndex;
  	private int tries = 0;
@@ -30,41 +30,52 @@ public class SimonSays extends KeyAdapter {
  	Date timeAtStart;
  	private  void makeAlbum() {
  // 2. add 4 images which match keyboard keys like this: images.put(new Integer(KeyEvent.VK_UP), "image.jpg");
-  	
+ 		images.put(new Integer(KeyEvent.VK_UP), "download.png");
+ 		images.put(new Integer(KeyEvent.VK_DOWN), "download-1.png");
+ 		images.put(new Integer(KeyEvent.VK_LEFT), "download-2.png");
+ 		images.put(new Integer(KeyEvent.VK_RIGHT), "download-3.png");
  // 3. Tell the user to "Press the matching key when 'Simon says' otherwise press a different key"
- 
+ System.out.println("Press the key when simon says, otherwise klkjhgf");
  // 4. call the method to show an image
-
+showImage();
  	}
 	public void keyPressed(KeyEvent e) {
     	int keyCode = e.getKeyCode();
     	// 16. make a points variable to track the score. tell the user their score at the end.
+    	int score = 0;
     	//17. if the keyCode matches the imageIndex and "Simon says..."  increase their score
+    	if(keyCode==imageIndex&&) {
+    		score++;
+    	}
+    	else()
     	//18.   if the keyCode doesn't match the imageIndex and "Simon didn't say..."  increase their score	
     	//19. Use the speak method to tell the user if they were correct or not
     	//13. increment tries by 1
-  	
+  	tries = tries + 1;
     	//14. if tries is greater than 9 (or however many you want)
-    	
+    	if(tries>9) {
+    		System.exit(0);
+    	}
     	//15.    	exit the program
 
     	//11. dispose of the frame
-   	
+   	frame.dispose();
     	//12. call the method to show an image
-
+showImage();
 	}
 	private void showImage() {
     	//5. initialize your frame to a new JFrame()
-	 
+	 frame = new JFrame();
     	//6. set the frame to visible
-	
+	frame.setVisible(true);
   	 //frame.add(getNextRandomImage()); //7. rename to the name of your frame
-    	
+	frame.add(getNextRandomImage());
     	// 8. set the size of the frame 
-     	
+     	frame.setSize(1000, 1000);
     	// 9. add a key listener to the frame
-	
+	frame.addKeyListener(null);
    	 //10. Use the speak method to either say "Simon says press this key" or "Press this key"
+	speak("Simon says press this key");
     	//Hint: use the simonSays int and a random number
 	}
 	private Component getNextRandomImage() {
---
+++ src/wack.java
@@ -0,0 +1,18 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class wack {
+	
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton button = new JButton();
+	frame.setSize(500, 500);
+	frame.setVisible(true);
+	panel.setVisible(true);
+	button.setVisible(true);
+	frame.pack();
+	
+	}
+}
